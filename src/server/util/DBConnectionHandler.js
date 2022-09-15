649f2b90a9d9834070656df430c215f802f2f7ca
slcao2 <steven.l.cao@gmail.com>
2022-09-14 23:34:42 -0500

    removing comments

--- src/server/util/DBConnectionHandler.js
+++ src/server/util/DBConnectionHandler.js
@@ -8,11 +8,11 @@ const createPool = () => {
   endPool();
   module.exports.pool = mysql.createPool({
     connectionLimit: 10,
-    host: process.env.DB_HOST, // '172.29.32.1'
-    user: process.env.DB_USER, // 'slcao2'
-    password: process.env.DB_PASSWORD, // 'password'
-    database: process.env.DB_DATABASE, // 'ouija'
-    port: process.env.DB_PORT, // 3306
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
     namedPlaceholders: true,
   }).promise();
 };
@@ -22,16 +22,6 @@ const endPool = () => {
     module.exports.pool.end();
   }
 };
-
-// const pool = mysql.createPool({
-//   connectionLimit: 10,
-//   host: '172.29.32.1', // '172.29.32.1'
-//   user: 'slcao2', // 'slcao2'
-//   password: 'password', // 'password'
-//   database: 'ouija', // 'ouija'
-//   port: 3306, // 3306
-//   namedPlaceholders: true,
-// }).promise();
 
 module.exports = {
   createPool,
